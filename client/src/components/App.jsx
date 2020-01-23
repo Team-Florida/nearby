e0f81fca6f05cf5cba8060a0ce6284b5b511a639
eggtart26 <justin.guan26@gmail.com>
2020-01-23 15:19:07 -0800

    init

---
+++ client/src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import MoreHouse from './MoreHouse.jsx'
+
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return (
+            <div>
+            <MoreHouse />
+            </div>
+        )
+    }
+}
+
+export default App;
